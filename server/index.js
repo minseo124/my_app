067d083cb9d2b5c9177080aebf56a9fe2fd692ca
minseo124 <dbalstj07@naver.com>
2024-07-18 14:43:04 +0900

    모듈설치 및 라우팅 추가

--- server/index.js
+++ server/index.js
@@ -4,5 +4,11 @@ const app = express();
 app.get('/', function (req, res) {
   res.sendFile(__dirname + "/public/home.html");
 });
+app.get('/main', function (req, res) {
+  res.sendFile(__dirname + "/public/main.html");
+});
+app.get('/contact', function (req, res) {
+  res.sendFile(__dirname + "/public/contact.html");
+});
 
 app.listen(3000);
