e8c4a06f3abeea371adfff050bda3c126a2a5aa2
minseo124 <dbalstj07@naver.com>
2024-07-24 17:00:57 +0900

    app.post 추가

--- server/index.js
+++ server/index.js
@@ -8,31 +8,54 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
+const users = [
+  {
+    idx: 1,
+    id: "test", 
+    pw: "1234",
+    created: new Date("2024-07-20"),
+    email: "test@naver.com",
+    nick: "겁나 무서운 전사",
+    cellphone: "010-1234-5678"
+  },
+  {
+    idx: 2,
+    id: "hello", 
+    pw: "hello1004",
+    created: new Date("2024-07-22"),
+    email: "helloworld@naver.com",
+    nick: "헬로월드",
+    cellphone: "010-1234-5678"
+  }
+];
 app.get('/', function (req, res) {
   res.sendFile(__dirname + "/public/home.html");
 });
 app.get('/main', function (req, res) {
   res.sendFile(__dirname + "/public/main.html");
 });
+app.post('/login', function(req,res) {
+  let {user_id, user_pw } = req.body; //각각 id, pw 정보를 나누어서 저장
+  console.log(user_id, user_pw);
+})
+// 기본 회원 정보와 전송 받은 정보를 비교 --> 일치 : 로그인 성공, 불일치 : 로그인 실패
+// 첫 페이지로 이동시키거나, 경고를 하거나..후속조치
 app.get('/chart', function(req, res) {
   res.json([
     {
-      ranking: 1,
+      ranking : 1,
       title: '데드풀과 울버린',
-      satisfy:99,
-      ratio: 15.7,
-      url:'/movie?cate=actions&sf?code={ranking}'
-    },
+      satisfy : 99,
+      ratio : 44.4,
+      },
     {
-      ranking: 2,
+      ranking : 2,
       title: '파일럿',
-      satisfy:99,
-      ratio: 15.7,
-      url:'/movie?cate=actions&sf?code={ranking}'
-    },
+      satisfy : 99,
+      ratio : 44.4,
+      }
   ])
-}
-)
+})
 // req : request [요청]
 // res : response [응답]
 app.post('/contact', function (req, res) {
