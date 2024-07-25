47a5a3f7e22cf4372c7bd4b73fc60ba913c3fbec
minseo124 <dbalstj07@naver.com>
2024-07-25 17:36:55 +0900

    0725

--- server/index.js
+++ server/index.js
@@ -6,12 +6,12 @@ app.use(express.static('public'))
 app.use(cors());
 /* request body로 전달되는 json/body를 express 서버가 이해할 수 있게 미들웨어를 등록한다 */
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 const users = [
   {
     idx: 1,
-    id: "test", 
+    id: "test",
     pw: "1234",
     created: new Date("2024-07-20"),
     email: "test@naver.com",
@@ -20,7 +20,7 @@ const users = [
   },
   {
     idx: 2,
-    id: "hello", 
+    id: "hello",
     pw: "hello1004",
     created: new Date("2024-07-22"),
     email: "helloworld@naver.com",
@@ -34,26 +34,42 @@ app.get('/', function (req, res) {
 app.get('/main', function (req, res) {
   res.sendFile(__dirname + "/public/main.html");
 });
-app.post('/login', function(req,res) {
-  let {user_id, user_pw } = req.body; //각각 id, pw 정보를 나누어서 저장
+app.post('/login', function (req, res) {
+  let { user_id, user_pw } = req.body; //각각 id, pw 정보를 나누어서 저장
   console.log(user_id, user_pw);
+  let foundUser = users.filter(user => user.id === user_id && user.pw === user_pw);
+  if (foundUser) {
+    res.status(200).json({
+      message: "login ok",
+      isLogin: true,
+      user: foundUser.nick,
+      redirect_url: "/home"
+    });
+  } else {
+    res.status(400).json({
+      message: "login fail",
+      isLogin: false,
+      user: null,
+      redirect_url: null
+    });
+  }
+  // 기본 회원 정보와 전송 받은 정보를 비교 --> 일치 : 로그인 성공, 불일치 : 로그인 실패
+  // 첫 페이지로 이동시키거나, 경고를 하거나..후속조치
 })
-// 기본 회원 정보와 전송 받은 정보를 비교 --> 일치 : 로그인 성공, 불일치 : 로그인 실패
-// 첫 페이지로 이동시키거나, 경고를 하거나..후속조치
-app.get('/chart', function(req, res) {
+app.get('/chart', function (req, res) {
   res.json([
     {
-      ranking : 1,
+      ranking: 1,
       title: '데드풀과 울버린',
-      satisfy : 99,
-      ratio : 44.4,
-      },
+      satisfy: 99,
+      ratio: 44.4,
+    },
     {
-      ranking : 2,
+      ranking: 2,
       title: '파일럿',
-      satisfy : 99,
-      ratio : 44.4,
-      }
+      satisfy: 99,
+      ratio: 44.4,
+    }
   ])
 })
 // req : request [요청]
